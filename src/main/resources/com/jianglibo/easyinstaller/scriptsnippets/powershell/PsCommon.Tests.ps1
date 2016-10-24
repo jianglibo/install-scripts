28af1f2511d52a76ebaef60e757f53fc5f73d7bf
libo <jianglibo@gmail.com>
2016-10-24 23:19:23 +0800

    add blockConfig file editor function

--- src/main/resources/com/jianglibo/easyinstaller/scriptsnippets/powershell/PsCommon.Tests.ps1
+++ src/main/resources/com/jianglibo/easyinstaller/scriptsnippets/powershell/PsCommon.Tests.ps1
@@ -6,4 +6,62 @@ Describe "PsCommon" {
     It "does something useful" {
         $true | Should Be $true
     }
+    It "should handle out context" {
+        $twoReached = $False
+        (1,2,3 | ForEach-Object {if ($_ -gt 1) {$twoReached = $True; $_}} | ForEach-Object {if ($twoReached) {$_}}).Count | Should Be 2
+    }
+    It "scriptmethod parameters" {
+        $oo = New-Object -TypeName PSObject
+        $oo | Add-Member -MemberType ScriptMethod -Name appp -Value {
+             Param([String]$fileToWrite)
+             $fileToWrite
+        }
+        $oo.appp("abaac") | Should Be "abaac" 
+    }
+    It "should addKv commentKv work" {
+        $kvf = New-SectionKvFile -FilePath (Join-Path $here -ChildPath "fixtures\NetworkManager.conf")
+        $ht = [HashTable]$kvf.blockHt;
+
+        $kvf.blockHt.Count | Should Be 2
+
+        $kvf.blockHt["[main]"] -contains "plugins=ifcfg-rh" | Should Be $True
+
+        $kvf.addKv("a", 1, "[main]")
+        $kvf.blockHt["[main]"] -contains "a=1" | Should Be $True
+
+        $kvf.blockHt["[main]"].Count | Should Be 3
+        $kvf.addKv("a", 1, "[main]")
+        $kvf.blockHt["[main]"].Count | Should Be 3
+
+        $kvf.commentKv("x", "[main]")
+        $kvf.blockHt["[main]"].Count | Should Be 3
+
+        $kvf.commentKv("a", "[main]")
+        $kvf.blockHt["[main]"] -contains "a=1" | Should Be $False
+        $kvf.blockHt["[main]"] -contains "#a=1" | Should Be $True
+
+        $kvf.addKv("a", 2, "[main]")
+        $kvf.blockHt["[main]"] -contains "a=1" | Should Be $False
+        $kvf.blockHt["[main]"] -contains "#a=1" | Should Be $False
+        $kvf.blockHt["[main]"] -contains "a=2" | Should Be $True
+        $kvf.blockHt["[main]"].Count | Should Be 3
+
+
+    }
+    It "should write to file work" {
+        $kvf = New-SectionKvFile -FilePath (Join-Path $here -ChildPath "fixtures\NetworkManager.conf")
+        $ht = [HashTable]$kvf.blockHt;
+        $ht.Keys | Should Be @("[main]", "[logging]")
+
+        ([Array]$kvf.prefix).Count| Should Be 12
+
+        $tmpf = (New-TemporaryFile).ToString()
+
+        $kvf.writeToFile($tmpf)
+
+        $line2 = Get-Content $tmpf | Select-Object -First 2
+
+        $line2 | Should Be @("# Configuration file for NetworkManager.", "#")
+        Remove-Item -Path $tmpf
+    }
 }
--- src/main/resources/com/jianglibo/easyinstaller/scriptsnippets/powershell/PsCommon.ps1
+++ src/main/resources/com/jianglibo/easyinstaller/scriptsnippets/powershell/PsCommon.ps1
@@ -31,3 +31,135 @@ function Run-Tar {
     $r = $command | Invoke-Expression | Where-Object {$_ -cmatch "Cannot open:.*"} | measure
     if ($r.Count -gt 0) {$false} else {$True}
 }
+
+function New-CentOs7Nm {
+ Param
+     (
+       [parameter(Mandatory=$False)]
+       [Boolean]
+       $EnableNetworkManager = $True,
+       [parameter(Mandatory=$False)]
+       [Boolean]
+       $EnableDns = $False
+    )
+    $nm = New-Object -TypeName PSObject
+    $d = [ordered]@{NetworkManagerEnabled=$EnableNetworkManager;DnsEnabled=$EnableDns}
+    $nm = $nm | Add-Member -NotePropertyMembers $d -PassThru
+    
+    $nm = $nm | Add-Member -MemberType ScriptMethod -Name init -Value {
+        if ($this.EnableNetworkManager) {
+            systemctl enable NetworkManager
+        } else {
+            systemctl disable NetworkManager
+        }
+    } -PassThru
+
+    return $nm
+}
+
+
+function New-SectionKvFile {
+ Param
+     (
+       [parameter(Mandatory=$True)]
+       [String]
+       $FilePath,
+       [parameter(Mandatory=$False)]
+       [String]
+       $SectionPattern = "^\[(.*)\]$",
+       [parameter(Mandatory=$False)]
+       [String]
+       $commentPattern = "^#.*"
+    )
+
+    $prefix = @()
+    $blockHt = [ordered]@{}
+    $blockStart = $False
+    $currentBlock = $null
+
+    Get-Content $FilePath | ForEach-Object {
+        if ($blockStart) {
+            if ($_ -match $SectionPattern) {
+                $currentBlock = $Matches[0]
+                $blockHt[$currentBlock] = @()
+            } else {
+                $blockHt[$currentBlock] += $_
+            }
+        } else {
+            if ($_ -match $SectionPattern) {
+                $blockStart = $True
+                $currentBlock = $Matches[0]
+                $blockHt[$currentBlock] = @()
+            }  else {
+                $prefix += $_
+            }
+        }
+    }
+
+    $skf = New-Object -TypeName PSObject -Property @{FilePath=$FilePath;blockHt=$blockHt;prefix=$prefix}
+
+    $addKv = {
+        param([String]$k, [String]$v, [String]$section)
+        $done = $False
+        $blockLines = $this.blockHt[$section] | ForEach-Object {
+            if ($done) {
+                $_
+            } else {
+                if ($_ -match "$k=") {
+                    $done = $True
+                    "$k=$v"
+                } else {
+                    $_
+                }
+            }
+        }
+
+        if (!$done) {
+            $blockLines += "$k=$v"
+        }
+        $this.blockHt[$section] = $blockLines
+    }
+
+    $skf = $skf | Add-Member -MemberType ScriptMethod -Name addKv -Value $addKv -PassThru
+
+     $commentKv = {
+        param([String]$k, [String]$section)
+        $done = $False
+        $blockLines = $this.blockHt[$section] | ForEach-Object {
+            if ($done) {
+                $_
+            } else {
+                if ($_ -match "^$k=") {
+                    $done = $True
+                    "#$_"
+                } else {
+                    $_
+                }
+            }
+        }
+        if ($done) { # if changed.
+            $this.blockHt[$section] = $blockLines
+        }
+    }
+
+    $skf = $skf | Add-Member -MemberType ScriptMethod -Name commentKv -Value $commentKv -PassThru
+
+    $writeToFile = {
+        param([parameter(Position=0,Mandatory=$False)][String]$fileToWrite)
+        if (!$fileToWrite) {
+            $fileToWrite = $this.FilePath
+        }
+        $lines = $this.prefix
+        ([HashTable]$this.blockHt).GetEnumerator().ForEach({
+            $lines += $_.Key
+            $lines += $_.Value
+        })
+        Set-Content -Path $fileToWrite -Value $lines
+    }
+
+    $skf = $skf | Add-Member -MemberType ScriptMethod -Name writeToFile -Value $writeToFile -PassThru
+
+    return $skf
+}
+
+# "1", "2" | Select-Object @{N="line"; E={$_}}, @{N="sstart"; E={$_ -eq "1"}}
