8f75ec001d799700f9e4da175b78b9f2121be1b7
libo <jianglibo@gmail.com>
2016-10-31 19:48:41 +0800

    working on hadoop scripts

--- hadoop/centos7-ps-2.7.3/src/code.ps1
+++ hadoop/centos7-ps-2.7.3/src/code.ps1
@@ -51,29 +51,28 @@ function Add-HadoopProperty {
 
 function Decorate-Env {
     Param([parameter(ValueFromPipeline=$True)]$myenv)
-    $myenv | Add-Member -MemberType ScriptProperty -Name zkconfigLines -Value {
-        $this.software.configContent.asHt("zkconfig").GetEnumerator() |
-            ForEach-Object {"{0}={1}" -f $_.Key,$_.Value} | Sort-Object
-    }
-    $myenv | Add-Member -MemberType ScriptProperty -Name serviceLines -Value {
-        $this.boxGroup.boxes |
-             Select-Object @{n="serverId"; e={$_.ip.split('\.')[-1]}}, hostname |
-             ForEach-Object {"server.{0}={1}:{2}:{3}" -f (@($_.serverId, $_.hostname) + $this.software.configContent.zkports.Split(','))} |
-             Sort-Object
-    }
-    $myenv | Add-Member -MemberType NoteProperty -Name DataDir -Value ($myenv.software.configContent.zkconfig.dataDir)
 
-    $myenv | Add-Member -MemberType NoteProperty -Name configFolder -Value (Split-Path -Parent $myenv.software.configContent.configFile)
-    $myenv | Add-Member -MemberType NoteProperty -Name configFile -Value $myenv.software.configContent.configFile
-    $myenv | Add-Member -MemberType NoteProperty -Name binDir -Value $myenv.software.configContent.binDir
-    $myenv | Add-Member -MemberType NoteProperty -Name logDir -Value $myenv.software.configContent.logDir
-    $myenv | Add-Member -MemberType NoteProperty -Name pidFile -Value $myenv.software.configContent.pidFile
-    $myenv | Add-Member -MemberType NoteProperty -Name logProp -Value $myenv.software.configContent.logProp
+    $nameNodeBox = $myenv.boxGroup.boxes | Where-Object {($_.roles -split ",") -contains "NameNode"} | Select-Object -First 1
+    $resourceManagerBox = $myenv.boxGroup.boxes | Where-Object {($_.roles -split ",") -contains "ResourceManager"} | Select-Object -First 1
+
+    $myenv | Add-Member -MemberType NoteProperty -Name defaultFS -Value ("hdfs://{0}:{1}" -f $nameNodeBox.hostname, $myenv.software.configContent.ports.namenode.api)
+    $myenv | Add-Member -MemberType NoteProperty -Name resourceManagerHostName -Value $resourceManagerBox.hostname
+    $myenv | Add-Member -MemberType NoteProperty -Name InstallDir -Value ($myenv.software.configContent.installDir)
     $myenv
 }
 
 function Install-Hd {
     Param($myenv)
+    if (!(Test-Path $myenv.InstallDir)) {
+        New-Item -Path $myenv.InstallDir -ItemType Directory | Out-Null
+    }
+
+    $tgzFile = $myenv.getUploadedFile("hadoop-.*\.tar\.gz")
+    if (Test-Path $tgzFile -PathType Leaf) {
+        Run-Tar $tgzFile -DestFolder $myenv.InstallDir
+    } else {
+        return
+    }
 }
 
 function Change-Status {
