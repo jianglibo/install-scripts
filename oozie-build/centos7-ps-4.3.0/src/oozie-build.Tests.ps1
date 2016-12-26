adb2eb68fc736d7b4662740e863fd104a10c067a
jianglibo <jianglibo@gmail.com>
2016-12-26 17:22:49 +0800

    working on oozie build

---
+++ oozie-build/centos7-ps-4.3.0/src/oozie-build.Tests.ps1
@@ -0,0 +1,45 @@
+﻿# $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$here = $PSScriptRoot
+$sut = (Split-Path -Leaf $PSCommandPath) -replace '\.Tests\.', '.'
+
+$testTgzFolder = Join-Path -Path $here -ChildPath "../../../tgzFolder" -Resolve
+$commonPath = Join-Path -Path $here -ChildPath "\..\..\..\src\main\resources\com\jianglibo\easyinstaller\scriptsnippets\powershell\PsCommon.Ps1" -Resolve
+. $commonPath
+. (Join-Path -Path $here -ChildPath "\..\..\..\src\main\resources\com\jianglibo\easyinstaller\scriptsnippets\powershell\CentOs7Util.Ps1" -Resolve)
+
+$envfile = $here | Split-Path -Parent | Join-Path -ChildPath fixtures/envforcodeexec.json -Resolve
+
+$I_AM_IN_TESTING = $True
+
+$resutl = . "$here\$sut" -envfile $envfile -action t
+
+Describe "code" {
+    It  "should build oozie by tar" {
+        return
+        $myenv = New-EnvForExec $envfile | ConvertTo-DecoratedEnv
+
+        $fn = "oozie-4.3.0.tar.gz"
+
+        $myenv.InstallDir | Should Be "/opt/oozie-build"
+
+        $tgzFile = Join-Path $here -ChildPath "../../../tgzFolder/$fn"
+
+        Test-Path $tgzFile -PathType Leaf | Should Be $True
+
+        $myenv.getUploadedFile("oozie-.*\.tar\.gz") | Should Be "/easy-installer/$fn"
+
+        $myenv.tgzFile = $tgzFile
+
+        Start-BuildOozieTar $myenv
+
+        $Error.Count | Should Be 0
+    }
+
+    It  "should build oozie by git" {
+        $myenv = New-EnvForExec $envfile | ConvertTo-DecoratedEnv
+
+        Start-BuildOozieGit $myenv
+
+        $Error.Count | Should Be 0
+    }
+}
---
+++ oozie-build/centos7-ps-4.3.0/src/oozie-build.ps1
@@ -0,0 +1,157 @@
+﻿# how to run this script. powershell -File /path/to/this/file.
+# ParamTest.ps1 - Show some parameter features
+# Param statement must be first non-comment, non-blank line in the script
+
+Param(
+    [parameter(Mandatory=$true)]$envfile,
+    [parameter(Mandatory=$true)]$action,
+    [string]$remainingArguments
+)
+
+# insert-common-script-here:powershell/PsCommon.ps1
+# insert-common-script-here:powershell/Centos7Util.ps1
+
+Get-Command java
+
+function ConvertTo-DecoratedEnv {
+    Param([parameter(ValueFromPipeline=$True)]$myenv)
+    $myenv | Add-Member -MemberType NoteProperty -Name InstallDir -Value ($myenv.software.configContent.installDir)
+    $myenv | Add-Member -MemberType NoteProperty -Name tgzFile -Value ($myenv.getUploadedFile("oozie-.*\.tar\.gz"))
+    $myenv
+}
+
+function Get-ZooieBuildDirInfomation {
+    Param($myenv)
+    $h = @{}
+    $h.mkdistro = Get-ChildItem $myenv.InstallDir -Recurse | Where-Object {($_.FullName -replace "\\", "/") -match "/bin/mkdistro\.sh$"} | Select-Object -First 1 -ExpandProperty FullName
+    $h.buildHome = $h.mkdistro | Split-Path -Parent | Split-Path -Parent
+    $h.tclFile = $h.buildHome | Join-Path -ChildPath "tcl.tcl"
+    $h
+}
+
+function Start-build {
+    Param($myenv)
+    $DirInfo = Get-ZooieBuildDirInfomation $myenv
+    $cmd =  $DirInfo.mkdistro + " -DskipTests"
+    $cmd | Write-HostIfInTesting
+
+    $j = Start-Job -ScriptBlock {$cmd | Invoke-Expression} 
+    $success = $false
+    for ($i = 0; $i -lt 1; $i++) {
+        Start-Sleep -Seconds 2
+        $j | Receive-Job -Keep | Write-HostIfInTesting
+        if ($j.State -in "Completed","Stopped","Failed") {
+            $j | Write-HostIfInTesting
+            $allr = $j | Receive-Job -Keep
+            "aaaaa" | Write-HostIfInTesting
+            $r = $allr | Where-Object {$_ -match "\[INFO\] Apache Oozie Distro .* SUCCESS"} | Select-Object -First 1
+            if ($r) {
+                $success = $true
+                break
+            } else {
+                $j = Start-Job -ScriptBlock {$cmd | Invoke-Expression}
+            }
+        }
+    }
+
+    # $mvnRoot = "~/.m2/repository/"
+    # $hadoopVersion = "2.4.0"
+    # $commonsConfigurationVersion = "1.8"
+    # $libext = "/home/jianglibo/ooziedistro/oozie-4.3.0/libext"
+    # $extjsUrl = "http://archive.cloudera.com/gplextras/misc/ext-2.2.zip"
+    # $extjsUrl -match "([^/]+)$"
+    # $extjs = $Matches[1]
+
+    # Get-ChildItem "${mvnRoot}org/apache/hadoop" -Recurse | Where-Object fullName -Match "/$hadoopVersion/" | Where-Object FullName -Match ".*\.jar$" | Where-Object FullName -NotMatch "(-sources\.jar|-tests\.jar)$" | Copy-Item -Destination $libext
+    # Get-ChildItem "${mvnRoot}commons-configuration" -Recurse | Where-Object FullName -Match "/$commonsConfigurationVersion/" | Where-Object FullName -Match ".*\.jar$" | Where-Object FullName -NotMatch "(-sources\.jar|-tests\.jar)$" | Copy-Item -Destination $libext
+    # Invoke-WebRequest -Uri $extjsUrl -OutFile $extjs
+    # Move-Item $extjs $libext
+
+
+    if (!$success) {
+        $allr | Write-Error 
+    }
+
+}
+function Start-BuildOozieGit {
+    Param($myenv)
+
+    if ("OOZIE_BUILD_SERVER" -notin $myenv.myRoles) {
+        Write-Output "this box has'nt a role of OOZIE_BUILD_SERVER, skipping installation"
+        return
+    }
+    $myenv.InstallDir | New-Directory
+    $cc = $myenv.software.configContent
+
+    if (!($cc.gitUrl -match "([^/]+)\.git$")) {
+        Write-Error -Message "gitUrl not end with .git."
+    }
+
+    $repoFolder = $myenv.InstallDir | Join-Path -ChildPath $Matches[1]
+    $repoFolder | Write-HostIfInTesting
+    if (Test-Path $repoFolder) {
+#        $repoFolder | Remove-Item -Recurse -Force
+    }
+
+    Set-Location $myenv.InstallDir
+#    git clone $cc.gitUrl
+    Set-Location $Matches[1]
+    git checkout $cc.branch
+    Start-build $myenv
+}
+
+function Start-BuildOozieTar {
+    Param($myenv)
+
+    if ("OOZIE_BUILD_SERVER" -notin $myenv.myRoles) {
+        Write-Output "this box has'nt a role of OOZIE_BUILD_SERVER, skipping installation"
+        return
+    }
+    $myenv.InstallDir | New-Directory
+    if (Test-Path $myenv.tgzFile -PathType Leaf) {
+        Start-Untgz $myenv.tgzFile -DestFolder $myenv.InstallDir | Out-Null
+    } else {
+        Write-Error ($myenv.tgzFile + " doesn't exists.")
+    }
+    Start-build $myenv
+}
+
+$myenv = New-EnvForExec $envfile | ConvertTo-DecoratedEnv
+
+if ("OOZIE_BUILD_SERVER" -notin $myenv.myRoles) {
+    if (!$I_AM_IN_TESTING) {
+        Write-Error "not a OOZIE_BUILD_SERVER"
+    }
+}
+
+switch ($action) {
+    "install-with-tar" {
+        Start-BuildOozieTar $myenv (ConvertFrom-Base64Parameter $remainingArguments)
+    }
+    "install-with-git" {
+        Start-BuildOozieTar $myenv (ConvertFrom-Base64Parameter $remainingArguments)
+    }
+    "t" {
+        # do nothing
+    }
+    default {
+        Write-Error -Message ("Unknown action {0}"  -f $action)
+    }
+}
+
+Write-SuccessResult
+
+#     $tcl=@"
+#     for {set index 0} { $index < 100 } { incr index } {
+#         if {[catch [exec {distroCmd} -DskipTests] o msg]} {
+#             continue
+#         } else {
+#             break
+#         }
+#     }
+# "@
+#     $tcl = $tcl -replace "distroCmd",$DirInfo.mkdistro
+
+#     $tcl | Write-HostIfInTesting
+#     $tcl | Out-File -FilePath $DirInfo.tclFile -Encoding ascii
+#     tclsh $DirInfo.tclFile
